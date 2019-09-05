554f9b8323b7bd2369cd9d86c64acccbbb22c3ae
Jinwen Yang <8589419+jwyang16@users.noreply.github.com>
2019-09-04 21:43:48 -0400

    add demo

--- R/demo.r
+++ R/demo.r
@@ -1,7 +1,7 @@
 #' Demo datasets
 #' including:
 #' bulk.sample: demo bulk ATAC-seq hematopoietic samples (137872 peaks by 8 samples)
-#' singlecell.dataset: demo scATAC-seq hematopoietic data (137872 peaks by 8 cells) 
+#' singlecell.dataset: demo scATAC-seq hematopoietic data (137872 peaks by 200 cells) 
 #' celltype.idx: true cell type labels of scATAC-seq cells
 #' @docType data
 #' @keywords demo datasets
